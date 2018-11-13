b07d97d5dd4bd5723d99c53c38ca2789eebb3fc0
Dushan Kumarasinghe <dushan.14@cse.mrt.ac.lk>
2018-11-14 00:39:51 +0530

    renamed app file

---
+++ modelserver/modelserver.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import predictor
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "iRoads trained model accessing API"
+
+
+@app.route('/getIri', methods=['POST'])
+def get_iri():
+    content = request.json
+    return predictor.predict(content)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
