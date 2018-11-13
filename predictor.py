47abf9f8159db2b8bfcd4cc5e52165ff23049880
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-11-13 21:32:35 +0530

    add model

---
+++ predictor.py
@@ -0,0 +1,16 @@
+from sklearn.externals import joblib
+import json
+
+import pandas as pd
+
+def predict(input):
+
+    df = pd.DataFrame(input["array"])
+    print(df)
+    joblib_file = "joblib_model-"+input["segment"]+".pkl"
+
+    model = joblib.load(joblib_file)
+    val_predictions = model.predict(df)
+    print(val_predictions)
+
+    return json.dumps(val_predictions.tolist())
