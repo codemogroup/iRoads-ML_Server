6f7cbda5d639ab123b3e2833f858365ca43296d7
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-11-14 18:45:20 +0530

    add segment models

--- predictor.py
+++ predictor.py
@@ -3,11 +3,12 @@ import json
 
 import pandas as pd
 
+
 def predict(input):
 
     df = pd.DataFrame(input["array"])
     print(df)
-    joblib_file = "joblib_model-"+input["segment"]+".pkl"
+    joblib_file = "./models/joblib_model-"+input["segment"]+".pkl"
 
     model = joblib.load(joblib_file)
     val_predictions = model.predict(df)
--- readme.md
+++ readme.md
@@ -5,3 +5,15 @@
 ### to run the application
 
 `python app.py`
+
+
+### sample request jason body
+
+{
+	"segment":"100",
+	"array":[
+		{"calcSpikesY":0,"calcSpikesX":1,"gpsSpeed":0},
+		{"calcSpikesY":260,"calcSpikesX":9,"gpsSpeed":5},
+		{"calcSpikesY":320,"calcSpikesX":5,"gpsSpeed":3}
+	]
+}
